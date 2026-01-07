4c69e4c3f684ea7cb89b55cfbe2d001e7e748451
emarekica <emarekica@gmail.com>
2026-01-07 20:30:02 +0100

    Hard-code initial layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 function App() {
   return (
     <>
-      <h1>Show me the money!</h1>
+      <h1>Random expenses tracker</h1>
+      <ul>
+        <li>
+          <div className="expenseItem">
+            <span>Book</span>
+            <span>€15</span>
+            <span>02.01.2026.</span>
+          </div>
+        </li>
+        <li>
+          <div className="expenseItem">
+            <span>Pencil</span>
+            <span>€5</span>
+            <span>04.01.2026.</span>
+          </div>
+        </li>
+        <li>
+          <div className="expenseItem">
+            <span>Notebook</span>
+            <span>€10</span>
+            <span>08.01.2026.</span>
+          </div>
+        </li>
+      </ul>
+      <button>Add random expense</button>
     </>
   );
 }
