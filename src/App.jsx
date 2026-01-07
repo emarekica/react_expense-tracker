247872d1487d35992998ceb4b19341d82e05b909
emarekica <emarekica@gmail.com>
2026-01-07 20:53:09 +0100

    Init style

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ function App() {
       <h1>Random expenses tracker</h1>
       <ul>
         <li>
-          <div className="expenseItem">
+          <div className="expense-item">
             <span>Book</span>
             <span>€15</span>
             <span>02.01.2026.</span>
           </div>
         </li>
         <li>
-          <div className="expenseItem">
+          <div className="expense-item">
             <span>Pencil</span>
             <span>€5</span>
             <span>04.01.2026.</span>
           </div>
         </li>
         <li>
-          <div className="expenseItem">
+          <div className="expense-item">
             <span>Notebook</span>
             <span>€10</span>
             <span>08.01.2026.</span>
